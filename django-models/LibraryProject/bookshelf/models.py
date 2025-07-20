b6b533ca588e154bfc5170cdbe881e26bad6b63b
Marto <martinkimani42@gmail.com>
2025-07-21 00:03:52 +0300

    fix models.py  and add class-based views in views.py

--- django-models/LibraryProject/bookshelf/models.py
+++ django-models/LibraryProject/bookshelf/models.py
@@ -1,10 +1,30 @@
 from django.db import models
 
-# Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # FK to Author
+    publication_year = models.IntegerField()
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)  # FK to Library
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name} ({self.publication_year})"
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)  # One-to-one with Library
+
+    def __str__(self):
+        return self.name
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-from bookshelf.models import Book 
+from django.shortcuts import render , get_object_or_404
+from bookshelf.models import Book , Library
 from django.views.generic.detail import DetailView
-from bookshelf.models import Library 
 
 #function-based view to display all books
 def book_list(request):
     books = Book.objects.all()
     return render(request,'list_books.html',{'books': books})
+
+# Class-based view to display details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
