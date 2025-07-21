eabe8db047744cbff82dcdca3e2a3220f41a5cfd
Marto <martinkimani42@gmail.com>
2025-07-21 18:32:52 +0300

    update settings to include login / logout url and redirect

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login redirect URL
+LOGIN_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = 'book-list'  # Redirect to book list after login
+LOGOUT_REDIRECT_URL = 'login'
