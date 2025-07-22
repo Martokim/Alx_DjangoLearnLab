f6a62818b09408bc57b8614770ddf8a69a220b4d
Marto <martinkimani42@gmail.com>
2025-07-22 18:25:06 +0300

    update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -31,8 +31,8 @@ urlpatterns = [
     path('member-area/', member_view, name='member-view'),
 
      # Book management
-    path('books/add/', add_book, name='add-book'),
-    path('books/<int:pk>/edit/', edit_book, name='edit-book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', delete_book, name='delete-book'),
 
     
