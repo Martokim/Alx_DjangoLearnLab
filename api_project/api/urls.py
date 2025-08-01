c01e5db12d10b892423545a6e671d61cf7a5acc4
Marto <martinkimani42@gmail.com>
2025-08-01 18:46:34 +0300

    add authentication tocken

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import BookList ,BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
 router =DefaultRouter()
@@ -7,5 +8,6 @@ router.register(r'books_all', BookViewSet,basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('auth-token/',obtain_auth_token, name='auth-token'),
     path('',include(router.urls))
- ]
+      ]
