e182e8e29fa041e4482c286ae1b992735a887b98
Marto <martinkimani42@gmail.com>
2025-08-06 19:41:54 +0300

    imporve url with crud functionality and some comments

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,34 +1,31 @@
 from django.urls import path
 from .views import (
-    BookListView,
-    BookDetailView,
-    BookCreateView,
-    BookUpdateView,
-    BookDeleteView,
+    BookListCreateView,
+    BookRetrieveUpdateDeleteView,
+    AuthorListCreateView,
+    AuthorRetrieveUpdateDeleteView,
 )
-from django.urls import path
-from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView
-
-# URL routing for CRUD operations on Book model:
-# - ListView: /books/ - List all books.
-# - DetailView: /books/<int:pk>/ - Retrieve a single book by ID.
-# - CreateView: /books/create/ - Create a new book (Authenticated users only).
-# - UpdateView: /books/update/<int:pk>/ - Update a book (Authenticated users only).
-# - DeleteView: /books/delete/<int:pk>/ - Delete a book (Authenticated users only).
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
-    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
+    
+    # BOOK ENDPOINTS    
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),  
+    # GET → List all books
+    # POST → Create a new book
+
+    path('books/<int:pk>/', BookRetrieveUpdateDeleteView.as_view(), name='book-detail'),  
+    # GET → Retrieve a book by ID
+    # PUT/PATCH → Update book
+    # DELETE → Delete book
+
+   
+    # AUTHOR ENDPOINTS
+     path('authors/', AuthorListCreateView.as_view(), name='author-list-create'),  
+    # GET → List all authors
+    # POST → Create new author
+
+    path('authors/<int:pk>/', AuthorRetrieveUpdateDeleteView.as_view(), name='author-detail'),  
+    # GET → Retrieve author details by ID
+    # PUT/PATCH → Update author
+    # DELETE → Delete author
 ]
-
-
-urlpatterns = [
-    path('books/',BookListView.as_view(), name='book-list'), # list all books
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),# get a book by ID
-    path('books/create/', BookCreateView.as_view(), name='book-create'), # create a new book
-    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'), 
-    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
-    ]
