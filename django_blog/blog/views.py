4ec979a27c3dd3aced6ea042e65823282c170529
Marto <martinkimani42@gmail.com>
2025-08-17 00:20:25 +0300

    add custom register/profile views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, ProfileForm
 
-# Create your views here.
+def register_vies(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful. You can now log in.')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})    
