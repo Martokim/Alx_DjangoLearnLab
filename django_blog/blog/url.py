174fd78c310a0a6317474d8569d3d3c93f150331
Marto <martinkimani42@gmail.com>
2025-08-17 00:13:56 +0300

    fix url patterns

--- django_blog/blog/url.py
+++ django_blog/blog/url.py
@@ -1,13 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register_view, profile_view
 
-urlpatterns =[
-
-    path('', auth_view.LoginView.as_view(template_name='blog/login.html'),name='home')
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'),name='logien')
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/', register_view, name='register'),
     path('profile/', profile_view, name='profile'),
-    
-    ]
+]
