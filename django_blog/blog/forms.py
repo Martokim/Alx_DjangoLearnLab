7a55eb66bd4550220fc0c76f8195f0c3cc11a9f3
Marto <martinkimani42@gmail.com>
2025-08-17 23:58:59 +0300

    add comment form

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Comment , Post
 
 class ResisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -21,8 +22,14 @@ class ProfileForm(forms.ModelForm):
         model = User
         fields = ('username', 'email')
 
-    # def save(self, commit=True):
-    #     user = super().save(commit=False)
-    #     if commit:
-    #         user.save()
-    #     return user
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['author_name', 'text']  
+
+    # Example validation rule
+    def clean_text(self):
+        text = self.cleaned_data.get('text')
+        if len(text) < 5:
+            raise forms.ValidationError("Comment must be at least 5 characters long.")
+        return text
