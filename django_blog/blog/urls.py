07a23c4ed8e48bcada42b55da89e65470c3fcde6
Marto <martinkimani42@gmail.com>
2025-08-17 23:19:44 +0300

    update urls to satisfy requirements

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from .views import register_view, login_view, logout_view, profile_view 
-
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 urlpatterns = [
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('profile/', profile_view, name='profile'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
