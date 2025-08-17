cfb1d6322b9445a9ffb04fc5440dc475c5aaf601
Marto <martinkimani42@gmail.com>
2025-08-17 23:16:35 +0300

    restructured views to satisfy requirments

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,39 +1,25 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
 
-# Register View
-def register_view(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # auto login after registration
-            return redirect("profile")
-    else:
-        form = UserCreationForm()
-    return render(request, "blog/register.html", {"form": form})
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
 
-# Login View
-def login_view(request):
-    from django.contrib.auth.forms import AuthenticationForm
-    if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect("profile")
-    else:
-        form = AuthenticationForm()
-    return render(request, "blog/login.html", {"form": form})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
 
-# Logout View
-def logout_view(request):
-    logout(request)
-    return redirect("login")
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
 
-# Profile View
-@login_required
-def profile_view(request):
-    return render(request, "blog/profile.html")
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/'
