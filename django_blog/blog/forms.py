5902a6820c3089484b545af17826f9e6b7e7d05c
Marto <martinkimani42@gmail.com>
2025-08-19 12:51:21 +0300

    fix spelling errors

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Comment , Post
-from taggit.models import TagWiget 
+from taggit.forms import TagWidget 
 
-class ResisterForm(UserCreationForm):
+class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
@@ -37,8 +37,8 @@ class CommentForm(forms.ModelForm):
 
 class PostForm(forms.ModelForm):
     class Meta:
-        model =Post
+        model = Post
         fields = ["title","content","tags"]
-        wigets = {
+        widgets = {
             "tags": TagWiget(),
         }
