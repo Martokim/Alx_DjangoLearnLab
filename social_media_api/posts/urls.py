48bef16f19ed8f56f790adacca394a86cff12bb3
Marto <martinkimani42@gmail.com>
2025-08-29 20:12:11 +0300

    update

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -8,10 +8,10 @@ from .views import (
 
 urlpatterns = [
     # --- Posts ---
-    path("posts/", PostListCreateView.as_view(), name="post-list-create"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("", PostListCreateView.as_view(), name="post-list-create"),
+    path("<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 
     # --- Comments ---
-    path("posts/<int:post_id>/comments/", CommentListCreateView.as_view(), name="comment-list-create"),
+    path("<int:post_id>/comments/", CommentListCreateView.as_view(), name="comment-list-create"),
     path("comments/<int:pk>/", CommentDetailView.as_view(), name="comment-detail"),
 ]
