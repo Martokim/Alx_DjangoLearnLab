8b2b7abac67d0422d58d7f7dbc88e906b55832e5
Marto <martinkimani42@gmail.com>
2025-08-29 20:22:00 +0300

    update urls

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),  
-    path("api/posts/", include("posts.urls")),   
+    path("posts/", include("posts.urls")),   
 
     # JWT authentication endpoints
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
