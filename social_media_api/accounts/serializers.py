fc135ee4be47b82778445fc5ee5b175f88d2035f
Marto <martinkimani42@gmail.com>
2025-08-29 19:25:20 +0300

    update serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model , authenticate
 from django.contrib.auth.password_validation import validate_password
-from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token 
+
+
 
 User = get_user_model()
 
@@ -31,7 +33,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["username", "email", "first_name", "last_name", "password", "password2"]
-
+    #validate if both passwords match
     def validate(self, attrs):
         if attrs["password"] != attrs["password2"]:
             raise serializers.ValidationError({"password": "Passwords must match."})
@@ -39,14 +41,18 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop("password2")
-        user = User.objects.create_user(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)
         return user
 
 
 class UserLoginSerializer(serializers.Serializer):
     """Serializer for logging in a user"""
     username = serializers.CharField(required=True)
-    password = serializers.CharField(write_only=True, required=True, style={"input_type": "password"})
+    password = serializers.CharField(
+        write_only=True, required=True, 
+        style={"input_type": "password"}
+        )
 
     def validate(self, attrs):
         username = attrs.get("username")
