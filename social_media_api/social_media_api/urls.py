a4233f51ca3a0a41d630cccc57b08d96956e3abc
Marto <martinkimani42@gmail.com>
2025-08-29 19:05:09 +0300

    Update jwt api routes

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,5 +1,13 @@
+# social_media_api/urls.py
+from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("api/accounts/", include("accounts.urls")),
+    path("admin/", admin.site.urls),
+    path("api/accounts/", include("accounts.urls")),  # include accounts app URLs
+
+    # JWT authentication endpoints
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
