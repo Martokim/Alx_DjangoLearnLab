f0abe857a2925ed1f33217cbe4dd1d884be2ab8e
Marto <martinkimani42@gmail.com>
2025-08-29 20:24:07 +0300

    update views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,33 +1,42 @@
-from rest_framework import serializers
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
+from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
-class PostListCreateView(generics.ListCreateAPIView):
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit/delete it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions only allowed to the owner
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    CRUD operations for Posts
+    """
     queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
-class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-
-class CommentListCreateView(generics.ListCreateAPIView):
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    CRUD operations for Comments
+    """
+    queryset = Comment.objects.all().order_by("-created_at")
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def get_queryset(self):
-        return Comment.objects.filter(post_id=self.kwargs["post_id"]).order_by("-created_at")
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user, post_id=self.kwargs["post_id"])
-
-
-class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+        serializer.save(author=self.request.user)
