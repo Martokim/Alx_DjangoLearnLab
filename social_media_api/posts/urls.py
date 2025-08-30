9f8e60944e03ed038e788b5e918661a94ffef8d0
Marto <martinkimani42@gmail.com>
2025-08-30 23:38:48 +0300

    add feed routes

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
-from .views import FollowUserView, UnfollowUserView  
+from django.urls import path
+from .views import like_post, unlike_post, feed
+
+urlpatterns = [
+    path('posts/<int:pk>/like/', like_post, name='like-post'),
+    path('posts/<int:pk>/unlike/', unlike_post, name='unlike-post'),
+    path('feed/', feed, name='feed')
+]
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
 
-urlpatterns = [
-    path("", include(router.urls)),
-
-    #  follow/unfollow routes
-    path("users/<int:user_id>/follow/", FollowUserView.as_view(), name="follow-user"),
-    path("users/<int:user_id>/unfollow/", UnfollowUserView.as_view(), name="unfollow-user"),
-]
+urlpatterns = router.urls
